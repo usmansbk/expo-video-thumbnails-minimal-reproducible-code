c7c376150fcdc3ac2037af3d6ec1e6b4dec445f5
usmansbk <usmansbk@gmail.com>
2024-09-26 10:40:35 +0100

    apply styling

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
 import { useCallback, useState } from "react";
-import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { Button, StyleSheet, Text, View, Image, TextInput } from "react-native";
 import * as VideoThumbnails from "expo-video-thumbnails";
 
-const videoUri =
+const defaultVideoUri =
   "https://github.com/user-attachments/assets/ec9a7752-6d21-43ae-a635-6205d3374db1";
 
 export default function App() {
+  const [videoUri, setVideoUri] = useState(defaultVideoUri);
+  const [imageUri, setImageUri] = useState(null);
   const [error, setError] = useState(null);
-  const [uri, setUri] = useState(null);
 
   const onPress = useCallback(async () => {
     try {
       const { uri } = await VideoThumbnails.getThumbnailAsync(videoUri);
-      setUri(uri);
+      setImageUri(uri);
     } catch (e) {
       console.warn(e);
       setError(e);
     }
-  }, []);
+  }, [videoUri]);
 
   return (
     <View style={styles.container}>
-      <Text>{videoUri}</Text>
+      <TextInput
+        style={styles.input}
+        multiline
+        value={videoUri}
+        onChangeText={setVideoUri}
+      />
       {error && (
-        <Text
-          selectable
-          style={{
-            marginVertical: 8,
-            color: "red",
-          }}
-        >
+        <Text selectable style={styles.error}>
           {error.message}
         </Text>
       )}
-      {uri && <Image source={{ uri }} style={styles.image} />}
+      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
       <Button title="Generate Thumbnail" onPress={onPress} />
     </View>
   );
@@ -49,5 +49,14 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
+  },
+  error: {
+    color: "red",
+    marginVertical: 8,
+  },
+  input: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 8,
   },
 });
