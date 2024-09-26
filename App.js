11d49de164609e555d552cd59cce0b14fdda09e0
usmansbk <usmansbk@gmail.com>
2024-09-26 10:34:29 +0100

    add assets

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useState } from "react";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import * as VideoThumbnails from "expo-video-thumbnails";
+
+const videoUri =
+  "https://github.com/user-attachments/assets/ec9a7752-6d21-43ae-a635-6205d3374db1";
 
 export default function App() {
+  const [error, setError] = useState(null);
+  const [uri, setUri] = useState(null);
+
+  const onPress = useCallback(async () => {
+    try {
+      const { uri } = await VideoThumbnails.getThumbnailAsync(videoUri);
+      setUri(uri);
+    } catch (e) {
+      console.warn(e);
+      setError(e);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{videoUri}</Text>
+      {error && (
+        <Text
+          selectable
+          style={{
+            marginVertical: 8,
+            color: "red",
+          }}
+        >
+          {error.message}
+        </Text>
+      )}
+      {uri && <Image source={{ uri }} style={styles.image} />}
+      <Button title="Generate Thumbnail" onPress={onPress} />
     </View>
   );
 }
@@ -13,8 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
